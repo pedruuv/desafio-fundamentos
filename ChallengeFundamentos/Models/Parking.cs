043eac020d2ba836508d897baa0800a7d99cfc7e
Pedro Vitor Gurgel <pedruuvtab@gmail.com>
2024-09-15 22:23:27 -0300

    challenge solved

---
+++ ChallengeFundamentos/Models/Parking.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeFundamentos.Models
+{
+    public class Parking
+    {
+        public decimal InitialPrice = 0;
+        public decimal PricePerHour = 0;
+        private List<string> Vehicles = new();
+
+        public Parking(decimal initialPrice, decimal pricePerHour)
+        {
+            this.InitialPrice = initialPrice;
+            this.PricePerHour = pricePerHour;
+        }
+
+        public void AddVehicle()
+        {
+            Console.WriteLine("Enter the license plate: ");
+            string licensePlate = Console.ReadLine();
+            Vehicles.Add(licensePlate);
+        }
+
+        public void RemoveVehicle()
+        {
+            Console.WriteLine("Enter the license plate: ");
+            string licensePlate = Console.ReadLine();
+
+           if(Vehicles.Any(v => v.ToUpper() == licensePlate.ToUpper()))
+            {
+                Console.WriteLine("Type the amount of time it was parked: ");
+                int hours = Convert.ToInt32(Console.ReadLine());
+                decimal price = InitialPrice + (PricePerHour * hours);
+
+                Vehicles.Remove(licensePlate);
+
+                Console.WriteLine($"The vehicle {licensePlate} was removed and the total price was R$ {price}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, this license plate is not parked here. Check if you typed correctly");
+            }
+        }
+
+        public void ListVehicles()
+        {
+            if (Vehicles.Any())
+            {
+                Console.WriteLine("The vehicles parked here are: ");
+                Vehicles.ForEach(Console.WriteLine);
+            }
+            else
+            {
+                Console.WriteLine("There are no vehicles parked here");
+            }
+        }
+
+
+    }
+}
---
+++ ChallengeFundamentos/Program.cs
@@ -0,0 +1,52 @@
+﻿using ChallengeFundamentos.Models;
+
+Console.WriteLine("Welcome to the Parking System");
+Console.WriteLine("Type the initial price: ");
+decimal initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Now, type the price per hour: ");
+decimal priceHour = Convert.ToDecimal(Console.ReadLine());
+
+Parking parking = new(initialPrice, priceHour);
+
+int option = 0;
+bool showMenu = true;
+
+while (showMenu)
+{
+    Menu();
+    option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            parking.AddVehicle();
+            break;
+        case 2:
+            parking.RemoveVehicle();
+            break;
+        case 3:
+            parking.ListVehicles();
+            break;
+        case 4:
+            showMenu = false;
+            break;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+    Console.WriteLine("Type any key to return: ");
+    Console.ReadKey();
+    Console.Clear();
+
+}
+Console.WriteLine("The application ended.");
+
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Type an option: ");
+    Console.WriteLine("1- Register Vehicle");
+    Console.WriteLine("2- Remove Vehicle");
+    Console.WriteLine("3- List Vehicles");
+    Console.WriteLine("4- Exit");
+}
